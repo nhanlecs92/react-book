35cd6c610f0096754c53eae5ba419d545fe1be9a
unknown <lvnhan2001@gmail.com>
2023-02-02 11:10:33 +0700

    Add voucher page.

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () =>{
+    return (
+        <nav className="nav-links mb-2 ">
+            <a href="/#" className=" capitalize text-[#c72468] hover:text-sky-600">Trang Chủ</a> {'>'} <a href="/#" className=" capitalize text-[#c72468] hover:text-sky-600">Voucher</a>
+        </nav>
+    )
+}
+
+export default Navbar;
